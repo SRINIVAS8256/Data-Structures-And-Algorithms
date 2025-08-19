e97e20591a01775e9f6281721cc0cc164318d52f
SRINIVAS D R <srinivasdr25@gmail.com>
2025-08-19 20:10:58 +0530

    bitwise operations description

---
+++ topics/bitwise_operations.py
@@ -0,0 +1,24 @@
+# complement(~)
+x=10
+X=-x-1 #formula to calculate compliment
+print(X) # -11 (complements are used to store negative values)
+
+#AND(&) Bitwise AND
+print(23 & 21)#21
+#when you & 1 with any number digits remain same
+
+#OR
+print(11|12) #15
+
+#XOR (only one should be true ) or (different binaries is = 1 example 1 ^ 0 = 1  )
+print(25 ^ 25) #0
+print(25 ^ 30) # 7
+
+#left shift(<<)
+#formula{ a<< 1= 2a     ,  a<<b = a * 2**b
+print(2<<1)#4  cuz doubled
+
+#right shift
+#formula{ a>>b = a / 2 ** b
+print(7>>1)#3
+
