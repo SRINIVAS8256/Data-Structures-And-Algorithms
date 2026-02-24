3c241e6702064913d59ffbee335263aa91a29c34
SRINIVAS D R <srinivasdr25@gmail.com>
2026-02-24 21:41:00 +0530

    patterns

--- topics/patterns.py
+++ topics/patterns.py
@@ -2,7 +2,7 @@ def main():
     test=2
 
     for test in range(0,test,1):
-        p7()
+        p10()
         print(" ")
 
 
@@ -90,6 +90,78 @@ def p7():
             print("*", end="")
 
         print()  # Move to next line
+            #    *
+            #   ***
+            #  *****
+            # *******
+
+
+def p8():
+    n = int(input(" enter number of rows n  = "))
+    for i in range(0,n,1):
+        for j in range(i):
+            print(" ",end=" ")
+        for k in range(2*n-(2*i+1)):
+            print("*",end=" ")
+        print(" ")
+            # * * * * * * *
+            #   * * * * *
+            #     * * *
+            #       *
+
+def p9():
+    n = int(input(" enter number of rows n  = "))
+    for i in range(n):
+        # Print spaces
+        for j in range(n - i - 1):
+            print(" ", end=" ")
+
+        # Print stars
+        for j in range(2 * i + 1):
+            print("*", end=" ")
+
+        print()
+    for i in range(0, n, 1):
+        for j in range(i):
+            print(" ", end=" ")
+        for j in range(2 * n - (2 * i + 1)):
+            print("*", end=" ")
+        print(" ")
+
+
+        # here we have merged two patterns 7 and 8
+
+            #       *
+            #     * * *
+            #   * * * * *
+            # * * * * * * *
+            # * * * * * * *
+            #   * * * * *
+            #     * * *
+            #       *
+
+def p10():
+    n=int(input("n = "))
+    for i in range(1,2*n,1):
+        stars=i
+        if i > n :
+            stars=2*n-i
+        for j in range(stars):
+            print("*",end=" ")
+        print()
+
+            # *
+            # * *
+            # * * *
+            # * * * *
+            # * * *
+            # * *
+            # *
+
+
+def p11():
+
+
 
 
 main()
